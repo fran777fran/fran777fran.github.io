34f6d2fdb043bd7098d271b40e242b2589763192
Francisco Sierra <franciscosierradelarosa@gmail.com>
2026-07-06 22:11:14 +0200

    Personalizar titulo y descripcion del sitio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Informática con Fran';
+export const SITE_DESCRIPTION = 'Documentando mi aprendizaje en sistemas, redes y desarrollo.';
