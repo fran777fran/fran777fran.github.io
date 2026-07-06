812eb86453e02f600124f5fa701fc78447fcfafa
Francisco Sierra <franciscosierradelarosa@gmail.com>
2026-07-06 19:40:45 +0200

    Configurar despliegue a GitHub Pages con Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+    site: 'https://fran777fran.github.io',
+	base: '/informatica-con-fran',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
